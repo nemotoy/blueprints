5592a292d5cd07ae3076f7fab516db06b4a192e6
Yuma Nemoto <nemoto1257@gmail.com>
2018-07-12 23:44:12 +0900

    NSQへのパブリッシュ

--- chapter5/socialpoll/twittervotes/main.go
+++ chapter5/socialpoll/twittervotes/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"log"
 
+	"github.com/nsqio/go-nsq"
+
 	"gopkg.in/mgo.v2"
 )
 
@@ -35,4 +37,19 @@ func loadOptions() ([]string, error) {
 	}
 	iter.Close()
 	return options, iter.Err()
+}
+
+func publishVotes(votes <-chan string) <-chan struct{} {
+	stopchan := make(chan struct{}, 1)
+	pub, _ := nsq.NewProducer("localhost:4150", nsq.NewConfig())
+	go func() {
+		for vote := range votes {
+			pub.Publish("votes", []byte(vote)) // 投票内容をパブリッシュ
+		}
+		log.Println("Publiser: 停止中です")
+		pub.Stop()
+		log.Println("Publiser: 停止しました")
+		stopchan <- struct{}{}
+	}()
+	return stopchan
 }
