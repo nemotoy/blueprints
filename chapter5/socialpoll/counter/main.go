5bb8e0204efaed652f8c892f0a8f4b2e98d47253
Yuma Nemoto <nemoto1257@gmail.com>
2018-07-14 21:23:16 +0900

    5.4.1-2 DB接続 NSQ受信

--- chapter5/socialpoll/counter/main.go
+++ chapter5/socialpoll/counter/main.go
@@ -3,7 +3,13 @@ package main
 import (
 	"flag"
 	"fmt"
+	"log"
 	"os"
+	"sync"
+
+	"github.com/nsqio/go-nsq"
+
+	mgo "gopkg.in/mgo.v2"
 )
 
 var fatalErr error
@@ -20,4 +26,39 @@ func main() {
 			os.Exit(1)
 		}
 	}()
+	log.Println("データベースに接続します...")
+	db, err := mgo.Dial("localhost")
+	if err != nil {
+		fatal(err)
+		return
+	}
+	defer func() {
+		log.Println("データベースを閉じます...")
+		db.Close()
+	}()
+	pollData := db.DB("ballots").C("Polls")
+
+	var countsLock sync.Mutex
+	var counts map[string]int
+
+	log.Println("NSQに接続します...")
+	q, err := nsq.NewConsumer("votes", "counter", nsq.NewConfig())
+	if err != nil {
+		fatal(err)
+		return
+	}
+	q.AddHandler(nsq.HandlerFunc(func(m *nsq.Message) error {
+		countsLock.Lock()
+		defer countsLock.Unlock()
+		if counts == nil {
+			counts = make(map[string]int)
+		}
+		vote := string(m.Body)
+		counts[vote]++
+		return nil
+	}))
+	if err := q.ConnectToNSQLookupd("localhost:4161"); err != nil {
+		fatal(err)
+		return
+	}
 }
