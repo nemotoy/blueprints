0038a173fe82761f7ba0d302b29c60288496b8b3
Yuma Nemoto <nemoto1257@gmail.com>
2018-07-14 21:40:37 +0900

    5.4.3 データベースを最新の状態に保つ

--- chapter5/socialpoll/counter/main.go
+++ chapter5/socialpoll/counter/main.go
@@ -6,13 +6,17 @@ import (
 	"log"
 	"os"
 	"sync"
+	"time"
 
 	"github.com/nsqio/go-nsq"
 
 	mgo "gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
 )
 
 var fatalErr error
+
+const updateDuration = 1 * time.Second
 
 func fatal(e error) {
 	fmt.Println(e)
@@ -61,4 +65,32 @@ func main() {
 		fatal(err)
 		return
 	}
+	log.Println("NSQ上での投票を待機します...")
+	var updater *time.Timer
+	updater = time.AfterFunc(updateDuration, func() {
+		countsLock.Lock()
+		defer countsLock.Unlock()
+		if len(counts) == 0 {
+			log.Println("新しい投票はありまえsん。データベースの更新をスキップします")
+		} else {
+			log.Println("データベースを更新します...")
+			log.Println(counts)
+			ok := true
+			for option, count := range counts {
+				sel := bson.M{"options": bson.M{"$in": []string{option}}}
+				up := bson.M{"$inc": bson.M{"results." + option: count}}
+				if _, err := pollData.UpdateAll(sel, up); err != nil {
+					log.Println("更新に失敗しました:", err)
+					ok = false
+					continue
+				}
+				counts[option] = 0
+			}
+			if ok {
+				log.Println("データベースの更新が完了しました")
+				counts = nil // reset
+			}
+		}
+		updater.Reset(updateDuration)
+	})
 }
