f17ee286c33996d27765167ecf7ca4626f5d8b02
Yuma Nemoto <nemoto1257@gmail.com>
2018-10-07 10:17:45 +0900

    7.3.5 add handler parse URL Query

--- chapter7/meander/cmd/main.go
+++ chapter7/meander/cmd/main.go
@@ -5,15 +5,30 @@ import (
 	"encoding/json"
 	"net/http"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
 	// runtime.GOMAXPROCS(runtime.NumCPU())
 	meander.APIKey = os.Getenv("GOOGLE_PLACES_API_KEY")
-	http.HandleFunc("/journeys", func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("/journeys", cors(func(w http.ResponseWriter, r *http.Request) {
 		respond(w, r, meander.Journeys)
-	})
+	}))
+	http.HandleFunc("/recommendations", cors(func(w http.ResponseWriter, r *http.Request) {
+		q := &meander.Query{
+			Journey: strings.Split(r.URL.Query().Get("journey"), "|"),
+		}
+		q.Lat, _ = strconv.ParseFloat(r.URL.Query().Get("lat"), 64)
+		q.Lng, _ = strconv.ParseFloat(r.URL.Query().Get("lng"), 64)
+		q.Radius, _ = strconv.Atoi(r.URL.Query().Get("radius"))
+		q.CostRangeStr = r.URL.Query().Get("const")
+		places := q.Run()
+		respond(w, r, places)
+	}))
+
 	http.ListenAndServe(":8080", http.DefaultServeMux)
+
 }
 
 func respond(w http.ResponseWriter, r *http.Request, data []interface{}) error {
@@ -22,4 +37,11 @@ func respond(w http.ResponseWriter, r *http.Request, data []interface{}) error {
 		publicData[i] = meander.Public(d)
 	}
 	return json.NewEncoder(w).Encode(publicData)
+}
+
+func cors(f http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		f(w, r)
+	}
 }
